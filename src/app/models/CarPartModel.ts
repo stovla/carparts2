cf1cfc7b4d94b4366042d1aa0e1e8b3c49a3bfc2
Vlastimir Radojevic <vlastimirsd@gmail.com>
2019-06-22 07:40:43 +0200

    added carpart model

---
+++ src/app/models/CarPartModel.ts
@@ -0,0 +1,9 @@
+export class CarPartModel {
+    id?: string;
+    instock: boolean;
+    partName: string;
+    partNumber: string;
+    price: number;
+    purchaseDate: Date;
+    webshop: string;
+}
